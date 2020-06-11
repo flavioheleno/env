3c6d4250ddb7f6523c04bc8d2d9d37a6331c1774
Flavio Heleno <flaviohbatista@gmail.com>
2020-06-10 21:54:47 -0300

    (+) Minor improvements here and there

--- test/ReadTest.php
+++ test/ReadTest.php
@@ -7,7 +7,7 @@ use Env\Read;
 use PHPUnit\Framework\TestCase;
 
 final class ReadTest extends TestCase {
-  public function testUpdateEnv() : void {
+  public function testUpdateEnv(): void {
     $val = Read::asString('testUpdate', 'defaultVal');
     $this->assertSame('defaultVal', $val);
 
@@ -18,84 +18,84 @@ final class ReadTest extends TestCase {
     $this->assertSame('updatedVal', $val);
   }
 
-  public function testReadAsStringDefaultValue() : void {
+  public function testReadAsStringDefaultValue(): void {
     $val = Read::asString('testAsString', 'defaultVal');
     $this->assertSame('defaultVal', $val);
   }
 
-  public function testReadAsString() : void {
+  public function testReadAsString(): void {
     putenv('testAsString=myVal');
 
     $val = Read::asString('testAsString', 'defaultVal');
     $this->assertSame('myVal', $val);
   }
 
-  public function testReadAsArrayDefaultValue() : void {
+  public function testReadAsArrayDefaultValue(): void {
     $val = Read::asArray('testAsArray', ['defVal1', 'defVal2']);
     $this->assertSame(['defVal1', 'defVal2'], $val);
   }
 
-  public function testReadAsArray() : void {
+  public function testReadAsArray(): void {
     putenv('testAsArray=myVal1,myVal2');
 
     $val = Read::asArray('testAsArray');
     $this->assertSame(['myVal1', 'myVal2'], $val);
   }
 
-  public function testReadAsReadIntegerDefaultValue() : void {
+  public function testReadAsReadIntegerDefaultValue(): void {
     $val = Read::asInteger('testAsInteger', 4321);
     $this->assertSame(4321, $val);
   }
 
-  public function testReadAsInteger() : void {
+  public function testReadAsInteger(): void {
     putenv('testAsInteger=1234');
 
     $val = Read::asInteger('testAsInteger');
     $this->assertSame(1234, $val);
   }
 
-  public function testReadAsFloatDefaultValue() : void {
+  public function testReadAsFloatDefaultValue(): void {
     $val = Read::asFloat('testAsFloat', 1.2);
     $this->assertSame(1.2, $val);
   }
 
-  public function testReadAsFloat() : void {
+  public function testReadAsFloat(): void {
     putenv('testAsFloat=0.4');
 
     $val = Read::asFloat('testAsFloat');
     $this->assertSame(0.4, $val);
   }
 
-  public function testReadAsBoolDefaultValue() : void {
+  public function testReadAsBoolDefaultValue(): void {
     $val = Read::asBool('testAsBool', false);
     $this->assertSame(false, $val);
   }
 
-  public function testReadAsBool() : void {
+  public function testReadAsBool(): void {
     putenv('testAsBool=1');
 
     $val = Read::asBool('testAsBool');
     $this->assertSame(true, $val);
   }
 
-  public function testRequiredFromJsonDefaultValue() : void {
+  public function testRequiredFromJsonDefaultValue(): void {
     $val = Read::fromJson('testFromJson', ['default' => 'value']);
     $this->assertSame(['default' => 'value'], $val);
   }
 
-  public function testReadFromJson() : void {
+  public function testReadFromJson(): void {
     putenv('testFromJson={"a":1,"b":"c"}');
 
     $val = Read::fromJson('testFromJson');
     $this->assertSame(['a' => 1, 'b' => 'c'], $val);
   }
 
-  public function testReadFromSerializedDefaultValue() : void {
+  public function testReadFromSerializedDefaultValue(): void {
     $val = Read::fromSerialized('testFromSerialized', ['default' => 'value']);
     $this->assertSame(['default' => 'value'], $val);
   }
 
-  public function testReadFromSerialized() : void {
+  public function testReadFromSerialized(): void {
     putenv('testFromSerialized=a:2:{s:1:"a";i:1;s:1:"b";s:1:"c";}');
 
     $val = Read::fromSerialized('testFromSerialized');
